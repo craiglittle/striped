667771a1b5c62627cf498deb02b47614190b74ec
Craig Little <craiglttl@gmail.com>
2013-04-13 21:14:58 -0700

    Streamline charge integration specs

--- spec/integration/charge_spec.rb
+++ spec/integration/charge_spec.rb
@@ -10,27 +10,27 @@ describe Striped do
         description: 'Charge for test@example.com'
       }
     }
-    subject(:create_charge) { Striped.charge.create(create_arguments) }
 
     before do
-      stub_post_with_auth('/charges').to_return(body: JSON.dump({amount: 400}))
-      create_charge
+      stub_post_with_auth('/charges')
+      Striped.charge.create(create_arguments)
     end
 
     it "sends the proper request to the Stripe API" do
       expect(a_post_with_auth('/charges').with(body: create_arguments)).to have_been_made
     end
-
-    it "returns a valid object" do
-      expect(create_charge.amount).to eq 400
-    end
   end
 
   context "when fetching a charge" do
-    before { Striped.charge('charge_id').fetch }
+    let(:charge_id) { 'charge_id' }
+
+    before do
+      stub_get_with_auth("/charges/#{charge_id}")
+      Striped.charge(charge_id).fetch
+    end
 
     it "sends the proper request to the Stripe API" do
-      expect(a_get_with_auth("/charges/charge_id")).to have_been_made
+      expect(a_get_with_auth("/charges/#{charge_id}")).to have_been_made
     end
   end
 end
