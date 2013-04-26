9a1dd6a42920978037e0e50776f2dc4337631b2a
Craig Little <craiglttl@gmail.com>
2013-04-25 23:01:18 -0700

    Add support for fetching invoice items

--- spec/integration/invoice_item_spec.rb
+++ spec/integration/invoice_item_spec.rb
@@ -1,6 +1,8 @@
 require 'integration/helper'
 
 describe Striped do
+  let(:invoice_item_id) { 'invoice_item_id' }
+
   context "when creating an invoice item" do
     let(:create_arguments) {
       {
@@ -18,6 +20,18 @@ describe Striped do
 
     it "sends the proper request to the Stripe API" do
       expect(a_post_with_auth('/invoiceitems').with(body: create_arguments))
+        .to have_been_made
+    end
+  end
+
+  context "when fetching an invoice item" do
+    before do
+      stub_get_with_auth("/invoiceitems/#{invoice_item_id}")
+      Striped.invoice_item(invoice_item_id).fetch
+    end
+
+    it "sends the proper request to the Stripe API" do
+      expect(a_get_with_auth("/invoiceitems/#{invoice_item_id}"))
         .to have_been_made
     end
   end
