d8fc7bbcf14433e2d11589a03280cd772fd609fd
Craig Little <craiglttl@gmail.com>
2013-04-25 23:08:26 -0700

    Add support for deleting invoice items

--- lib/striped/proxy/invoice_item.rb
+++ lib/striped/proxy/invoice_item.rb
@@ -4,7 +4,7 @@ require 'striped/proxy/operations'
 module Striped
   module Proxy
     class InvoiceItem < Base
-      include Striped::Proxy::Operations :create, :fetch, :update
+      include Striped::Proxy::Operations :create, :fetch, :update, :delete
     end
   end
 end
--- spec/integration/invoice_item_spec.rb
+++ spec/integration/invoice_item_spec.rb
@@ -56,4 +56,16 @@ describe Striped do
       ).to have_been_made
     end
   end
+
+  context "when deleting an invoice item" do
+    before do
+      stub_delete_with_auth("/invoiceitems/#{invoice_item_id}")
+      Striped.invoice_item(invoice_item_id).delete
+    end
+
+    it "sends the proper request to the Stripe API" do
+      expect(a_delete_with_auth("/invoiceitems/#{invoice_item_id}"))
+        .to have_been_made
+    end
+  end
 end
